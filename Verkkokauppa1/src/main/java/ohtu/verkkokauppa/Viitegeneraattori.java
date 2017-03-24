add6514c3941c6cc023d74ab0c1bc87d996a0a69
Camilla Colliander <camilla.colliander@gmail.com>
2017-03-24 16:00:18 +0200

    korjailuja

--- Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Viitegeneraattori.java
+++ Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Viitegeneraattori.java
@@ -1,23 +1,23 @@
 package ohtu.verkkokauppa;
 
-public class Viitegeneraattori {
+public class Viitegeneraattori implements ViitegeneraattoriRajapinta {
 
-    private static Viitegeneraattori instanssi;
+//    private static Viitegeneraattori instanssi;
 
-    public static Viitegeneraattori getInstance() {
-        if (instanssi == null) {
-            instanssi = new Viitegeneraattori();
-        }
-
-        return instanssi;
-    }
-    
+//    public static Viitegeneraattori getInstance() {
+//        if (instanssi == null) {
+//            instanssi = new Viitegeneraattori();
+//        }
+//
+//        return instanssi;
+//    }
     private int seuraava;
     
-    private Viitegeneraattori(){
+    public Viitegeneraattori(){
         seuraava = 1;    
     }
     
+    @Override
     public int uusi(){
         return seuraava++;
     }
