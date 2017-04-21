f169ff3b36dac5683b1822990b3bd3260d14f672
Camilla Colliander <camilla.colliander@gmail.com>
2017-04-21 12:51:17 +0300

    teht 5 tehty jipii

--- ohtu-viikko5/Tennis/src/main/java/ohtu/TennisGame.java
+++ ohtu-viikko5/Tennis/src/main/java/ohtu/TennisGame.java
@@ -3,60 +3,58 @@ package ohtu;
 import java.util.HashMap;
 
 public class TennisGame {
-
-    private HashMap<Integer, String> pointToText;
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+    private int p1score = 0;
+    private int p2score = 0;
     private String player1Name;
     private String player2Name;
+    private HashMap<Integer,String> scoreToText;
 
     public TennisGame(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
-        pointToText.put(0, "Love");
-        pointToText.put(1, "Fifteen");
-        pointToText.put(2, "Thirty");
-        pointToText.put(3, "Forty");
+        scoreToText = new HashMap();
+        scoreToText.put(0,"Love");
+        scoreToText.put(1,"Fifteen");
+        scoreToText.put(2,"Thirty");
+        scoreToText.put(3,"Forty");
+
     }
 
     public void wonPoint(String playerName) {
         if (playerName.equals(player1Name)) {
-            m_score1++;
-        } else if(playerName.equals(player2Name)){
-            m_score2++;
+          p1score++;
         }
-    }
-
-    public String resultCounter() {
-        int result = m_score1 - m_score2;
-        if (result == 1) {
+        else if (playerName.equals(player2Name)) {
+          p2score++;
+        }
+     }
+    public String getResult(){
+         int result = p1score - p2score;
+          if (result == 1) {
             return "Advantage " + player1Name;
-        }
-        if (result == -1) {
+          }
+          if (result == -1) {
             return "Advantage " + player2Name;
-        }
-        if (result >= 2) {
+          }
+          if (result >= 2) {
             return "Win for " + player1Name;
-        }
-
-        return "Win for " + player2Name;
-
+          }
+          
+            return "Win for " + player2Name;
+          
     }
 
     public String getScore() {
-     
 
-        if (m_score1 == m_score2) {
-            if (m_score1 < 4) {
-                return pointToText.get(m_score1) + "-All";
+        if (p1score == p2score) {
+            if (p1score < 4) {
+              return scoreToText.get(p1score) + "-All";
             }
             return "Deuce";
-        
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
-            return resultCounter();
-
         }
-        return pointToText.get(m_score1) + "-" + pointToText.get(m_score2);
-
+        else if (p1score >= 4 || p2score >= 4) {
+          return getResult();
+        }
+          return scoreToText.get(p1score) + "-" + scoreToText.get(p2score);
     }
 }
